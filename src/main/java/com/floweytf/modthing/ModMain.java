5f47453a7f5b91cbe3d95ead1c5ec79f69d9b34a
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2021-02-23 14:12:22 -0600

    fixed stuffs

--- src/main/java/com/floweytf/modthing/ModMain.java
+++ src/main/java/com/floweytf/modthing/ModMain.java
@@ -5,12 +5,20 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.loader.FabricLoader;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 import java.nio.file.Path;
 
@@ -24,6 +32,18 @@ public class ModMain implements ModInitializer {
     public static double decayRate = .5;
     public static int radius = 16;
 
+    private static ConfiguredFeature<?, ?> ORE_ATHANITE_OVERWORLD = Feature.ORE
+            .configure(new OreFeatureConfig(
+                    OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                    Blocks.WHITE_WOOL.getDefaultState(),
+                    1)) // vein size
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
+                    0,
+                    0,
+                    64)))
+            .spreadHorizontally()
+            .repeat(20);
+
     @Override
     public void onInitialize() {
         Registry.register(Registry.BLOCK, new Identifier("athanite", "athanite_block"), ATHANITE_BLOCK_BLOCK);
@@ -32,5 +52,9 @@ public class ModMain implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("athanite", "athanite_nugget"), ATHANITE_NUGGET);
         Registry.register(Registry.ITEM, new Identifier("athanite", "athanite_block"), ATHANITE_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier("athanite", "athanite_ore"), ATHANITE_ORE_ITEM);
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreWoolOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
+                new Identifier("athanite", "ore_athanite_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), ORE_ATHANITE_OVERWORLD);
     }
 }
